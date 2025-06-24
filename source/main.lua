f535a3c13eb1e2b14e2941cfec5b6a0d9d366757
Jason Kisner <jasonkisner@gmail.com>
2025-06-24 11:56:35 -0400

    feat(Scene): import integration test scenes in main.lua
    Add SceneIntegrationTest, TransitionIntegrationTest, and 
    SequenceCIntegrationTest to main.lua and update the default scene 
    to enable running integration tests.

--- source/main.lua
+++ source/main.lua
@@ -9,9 +9,15 @@ pd.display.setRefreshRate(30)
 
 import "scenes/SceneTest"
 import "scenes/TransitionTest"
+import "scenes/SceneIntegrationTest"
+import "scenes/TransitionIntegrationTest"
+import "scenes/SequenceCIntegrationTest"
 r.Scene.registerScenes({
   SceneTest = SceneTest,
-  TransitionTest = TransitionTest
+  TransitionTest = TransitionTest,
+  SceneIntegrationTest = SceneIntegrationTest,
+  TransitionIntegrationTest = TransitionIntegrationTest,
+  SequenceCIntegrationTest = SequenceCIntegrationTest
 })
 
-r.new(SceneTest)
+r.new(SceneIntegrationTest)
