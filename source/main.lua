c4af518f64cf20dcbda12d12acbc061cf2acc911
Jason Kisner <jasonkisner@gmail.com>
2025-06-25 19:43:50 -0400

    feat(main): set InputIntegrationTest as default scene for startup
    Import InputIntegrationTest and make it the starting scene in main.lua 
    to enable direct testing of input manager functionality at launch.

--- source/main.lua
+++ source/main.lua
@@ -9,15 +9,17 @@ pd.display.setRefreshRate(30)
 
 import "scenes/SceneTest"
 import "scenes/TransitionTest"
+import "scenes/InputIntegrationTest"
 import "scenes/SceneIntegrationTest"
 import "scenes/TransitionIntegrationTest"
 import "scenes/SequenceCIntegrationTest"
 r.Scene.registerScenes({
   SceneTest = SceneTest,
   TransitionTest = TransitionTest,
+  InputIntegrationTest = InputIntegrationTest,
   SceneIntegrationTest = SceneIntegrationTest,
   TransitionIntegrationTest = TransitionIntegrationTest,
   SequenceCIntegrationTest = SequenceCIntegrationTest
 })
 
-r.new(SceneIntegrationTest)
+r.new(InputIntegrationTest)
